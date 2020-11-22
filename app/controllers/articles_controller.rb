5bdf581836bf3e73ce56a5889963b9747f57c820
serkansahin55 <ssahin55@outlook.com>
2020-11-22 18:44:44 -0500

    finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,5 +19,23 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])  
+  end
+  
+  def update
+    @article = Article.find(params[:id]) 
+    @article.update(article_params)
+    
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+
   # add edit and update methods here
 end
